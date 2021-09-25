5a86ec4c3e0463b6d45db3c62ee5e4402fde1ffd
JOANITA-51 <82649346+JOANITA-51@users.noreply.github.com>
2021-09-26 02:02:40 +0300

    adding more comments

--- sum-time.py
+++ sum-time.py
@@ -3,12 +3,12 @@ N = 1000000000
 sum = 0 #initialising the sum
 # Adding a header to the table
 print("{:<4}{:<8}{:<8}".format('N','Sum','Running Time')) 
-# loop from 0 to N
+# loop from 0 to N taking a step of 1. Display the current number, sum and the Running time
 for num in range(0, N , 1):
-    sum = sum + num
-     # end = '' avoids printing on a new line
+    sum = sum + num # Calculation the sum . i.e new sum = old sum + current number
+    # end = '' avoids printing on a new line
     print(num , '   ', end = '' ) # displays the current number
-    print(sum, '      ', end = '') # displays the sum of the current number and the numbers before it
+    print(sum, '      ', end = '') # displays the new sum 
     print(num+1) # displays the running time 
 print("Sum of first ", N, "numbers is: ", sum)   
 print("It takes N times to run hence time complexity O(N)")
